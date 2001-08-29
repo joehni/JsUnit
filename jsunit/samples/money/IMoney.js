740a8b3affad9b7332b4110bd18b8b6ecc355d3e
Jörg Schaible <joerg.schaible@gmx.de>
2001-08-29 19:15:47 +0000

    Add copyright note
    git-svn-id: svn://bobbel/repos/trunk@33 d4f5e813-a214-0410-99c4-8f31caba3383

--- jsunit/samples/money/IMoney.js
+++ jsunit/samples/money/IMoney.js
@@ -1,3 +1,25 @@
+/*
+JsUnit - a JUnit port for JavaScript
+Copyright (C) 1999-2001 Joerg Schaible
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation in version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Test suites built with JsUnit are derivative works derived from tested 
+classes and functions in their production; they are not affected by this 
+license.
+*/
+
 /**
  * The common interface for simple Monies and MoneyBags
  *
--- jsunit/samples/money/MoneyBag.js
+++ jsunit/samples/money/MoneyBag.js
@@ -1,3 +1,25 @@
+/*
+JsUnit - a JUnit port for JavaScript
+Copyright (C) 1999-2001 Joerg Schaible
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation in version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Test suites built with JsUnit are derivative works derived from tested 
+classes and functions in their production; they are not affected by this 
+license.
+*/
+
 /**
  * A MoneyBag defers exchange rate conversions. For example adding 
  * 12 Swiss Francs to 14 US Dollars is represented as a bag 
