86ad3e483aec6e8a9e19e315765dab9764680066
Jörg Schaible <joerg.schaible@gmx.de>
2001-09-01 21:20:37 +0000

    support Rhino of Mozilla.org
    git-svn-id: svn://bobbel/repos/trunk@39 d4f5e813-a214-0410-99c4-8f31caba3383

---
+++ jsunit/tests/JsUnitRhinoTest.js
@@ -0,0 +1,48 @@
+/*
+JsUnit - a JUnit port for JavaScript
+Copyright (C) 1999-2001 Joerg Schaible
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation in version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Test suites built with JsUnit are derivative works derived from tested 
+classes and functions in their production; they are not affected by this 
+license.
+*/
+
+load( 
+	"../lib/JsUnit.js", 
+	"../lib/JsUnitRhino.js", 
+	"../lib/CallStack.js",
+	"CallStackTest.js",
+	"JsUnitTest.js" );
+
+function main( args )
+{
+	var runner = new RhinoShellTestRunner();
+	runner.addSuite( new TestSuite( new CallStackTest()));
+	runner.addSuite( new TestSuite( new AssertionFailedErrorTest()));
+	runner.addSuite( new TestSuite( new TestTest()));
+	runner.addSuite( new TestSuite( new TestFailureTest()));
+	runner.addSuite( new TestSuite( new TestResultTest()));
+	runner.addSuite( new TestSuite( new AssertTest()));
+	runner.addSuite( new TestSuite( new TestCaseTest()));
+	runner.addSuite( new TestSuite( new TestSuiteTest()));
+	runner.addSuite( new TestSuite( new TestRunnerTest()));
+	runner.addSuite( new TestSuite( new TextTestRunnerTest()));
+
+	return runner.start( main.arguments );
+}
+
+quit( main( arguments ));
+
