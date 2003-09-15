426218127cdad6f7d0c593d61bea6945e10b82c3
Jörg Schaible <joerg.schaible@gmx.de>
2003-09-15 19:46:29 +0000

    Running IE from command line
    git-svn-id: svn://bobbel/repos/branches/JsUnit_1-2_branch@154 d4f5e813-a214-0410-99c4-8f31caba3383

---
+++ jsunit/samples/IE.js
@@ -0,0 +1,44 @@
+/*
+JsUnit - a JUnit port for JavaScript
+Copyright (C) 1999,2000,2001,2002 Joerg Schaible
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation in version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Test suites built with JsUnit are derivative works derived from tested 
+classes and functions in their production; they are not affected by this 
+license.
+*/
+
+var shell = WScript.CreateObject ("WScript.Shell");
+var ie = WScript.CreateObject("InternetExplorer.Application");
+ie.Visible = true;
+ie.Navigate("file://" + shell.CurrentDirectory + "/AllTests.html");
+
+var count = 0;
+while( count < 5 )
+{
+	if( ie.ReadyState == 4 )
+		break;
+	WScript.sleep(500);
+}
+
+var text = ie.Document.getElementsByTagName("body")[0].innerHTML;
+
+ie.Quit();
+
+if( text.match(/0 errors/) && text.match(/0 failures/))
+	WScript.Quit(0);
+	
+WScript.Quit(1);
+
